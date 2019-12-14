a4c5d3e4210c637312f5124e32aa0a52385d99ff
Stephen Harris <contact@stephenharris.info>
2019-12-14 18:31:50 +0000

    import config module to get access to APPLICATION_ENV config when instantiating dynamodb client

--- src/lists/lists.module.ts
+++ src/lists/lists.module.ts
@@ -1,20 +1,23 @@
 import { Module, NestModule} from '@nestjs/common';
 import { ListItemsController } from './controllers/lists.controller';
 import { ItemService } from './services/items.service';
+import { ConfigService } from '../config/services/config.service';
+import { ConfigModule } from '../config/config.module';
 const AWS = require('aws-sdk');
 
 const dynamodbClientFactory = {
     provide: 'DYNAMODB_CLIENT',
-    useFactory: () => {
-      if ('development' === process.env.APPLICATION_ENV) {
+    useFactory: async (config) => {
+      if ('development' === await config.get("APPLICATION_ENV")) {
         AWS.config.loadFromPath('./config.local.json');
       }
       return new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
-    }
+    },
+    inject:[ConfigService]
   };
 
 @Module({
-    imports: [],
+    imports: [ConfigModule],
     controllers: [ListItemsController],
     providers: [
         ItemService,
